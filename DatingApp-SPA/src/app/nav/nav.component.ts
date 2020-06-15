5d1266927ce6815b6569dbe99892e77f18c59e90
unknown <filzapasha91@gmail.com>
2020-06-15 18:20:39 -0500

    Error handling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged in successful');
     }, error => {
-      console.log('Logged in failed');
+      console.log(error);
     });
   }
 
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -20,9 +20,8 @@ export class RegisterComponent implements OnInit {
     this.authService.register(this.model).subscribe(() => {
       console.log('Registration successful');
     }, error => {
-      console.log('Registration failed');
+      console.log(error);
     });
-    console.log(this.model);
   }
 
   cancel() {
